82e95f3c066a6b7b29a7ab4de587d43e43422828
tattloong <tattloongpoh@gmail.com>
2018-04-25 17:57:51 +0800

    testing

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,33 @@
+class ListingsController < ApplicationController
+
+	def index
+
+	end
+
+	def create
+
+	end
+
+	def new
+		
+		@listing = Listing.new
+
+	end
+
+	def edit
+
+	end
+
+	def show
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
+
+end
