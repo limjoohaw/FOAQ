dd48f12ad3cd25398ee9257150d0fe3fea88191a
Albert Tan <tch_albert@hotmail.com>
2018-04-25 14:46:44 +0800

    added columns into listings table

--- db/migrate/20180424092437_create_listings.rb
+++ db/migrate/20180424092437_create_listings.rb
@@ -1,6 +1,12 @@
 class CreateListings < ActiveRecord::Migration[5.1]
   def change
     create_table :listings do |t|
+			t.string :questions
+			t.text :answers
+			t.index :votes
+			t.string :headline
+    	t.string :person_of_interest
+
 
       t.timestamps
     end
